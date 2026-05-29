05a48227605e4c7cbaa416b25f9d35c016ee2827
Darenv-cloud <darenv@hyve.com>
2026-05-29 15:32:11 +0100

    build

---
+++ octofit-tracker/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container mt-4">
+      <h1>OctoFit Tracker</h1>
+    </div>
+  )
+}
+
+export default App
